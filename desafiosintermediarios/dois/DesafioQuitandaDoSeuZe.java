45abcfc6aa5620f50172f87b061db71bd661f0b6
Carloscam98 <morato.acarlos@gmail.com>
2023-03-11 12:47:04 -0300

    Padrões de projeto com projeto agencia bancaria

--- desafiosintermediarios/dois/DesafioQuitandaDoSeuZe.java
+++ desafiosintermediarios/dois/DesafioQuitandaDoSeuZe.java
@@ -1,4 +1,36 @@
 package dio.java.pan.desafiosintermediarios.dois;
+
+import java.util.*;
 public class DesafioQuitandaDoSeuZe{
 
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        int morangos = input.nextInt();
+        int macas = input.nextInt();
+
+        double totalMorangos, totalMacas, totalCompra;
+
+        if(morangos <= 5){
+            totalMorangos = morangos * 2.5;
+        }else{
+            totalMorangos = morangos * 2.2;
+        }
+
+        if(macas <= 5){
+            totalMacas = macas * 1.8;
+        }else{
+            totalMacas = macas * 1.5;
+        }
+
+        if((totalMacas + totalMorangos) > 25.0 || (morangos + macas) > 8 ){
+
+            totalCompra = (totalMorangos + totalMacas) * 0.9;
+        }else{
+            totalCompra = totalMorangos + totalMacas;
+        }
+        System.out.println(totalCompra);
+    }
 }
